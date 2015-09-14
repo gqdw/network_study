3858b355f7c837b84e7b08352009f70891c22306
gqdw <aca_jingru@qq.com>
2015-09-14 09:42:15 +0800

    book version

---
+++ udp_local.py
@@ -0,0 +1,21 @@
+import socket,sys
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+max = 65535
+port = 1060
+print sys.argv[1:]
+if sys.argv[1:] == ['server']:
+	s.bind(('127.0.0.1',port))
+	print 'listening at',s.getsockname()
+	while True:
+		data,address = s.recvfrom(max)
+		print 'The client at', address, 'says', repr(data)
+		s.sendto('Your data was %d bytes' % len(data), address)
+elif sys.argv[1:] == ['client']:
+	print 'Address before sending:', s.getsockname()
+	s.sendto('This is my message', ('127.0.0.1', port))
+	print 'Address after sending', s.getsockname()
+	data, address = s.recvfrom(max) # overly promiscuous - see text!
+	print 'The server', address, 'says', repr(data)
+else:
+	print >>sys.stderr, 'usage: udp_local.py server|client'
